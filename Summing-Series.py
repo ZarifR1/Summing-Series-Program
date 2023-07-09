6de060b3a8b04bd2c49024eb8186f76d47bb9412
ZarifR1 <zarif.rahman1@education.nsw.gov.au>
2023-07-09 19:41:45 +1000

    First Basic Iteration of the Working Program

--- Summing-Series.py
+++ Summing-Series.py
@@ -29,10 +29,43 @@ entry1.place(x=200,y=50)
 entry2.place(x=200,y=100)
 entry3.place(x=200,y=150)
 
+#Function to calculate the sum and series of an ap
+def ap():
+    a=float(entry1.get())                                       #a=first term
+    d=float(entry2.get())                                       #d=increment
+    n=float(entry3.get())                                       #n=number of terms
+    f=0                                                         #the 'fth' term in the series
+    series=[]                                                   #empty series
+    series.append(a+f*d)                                        #first term
+    while (f+1) != n:                                           #goes through the terms
+        f=f+1                                                   #increments the terms
+        series.append(",")                                      #separates the terms
+        series.append(a+f*d)                                    #term inputted in the series
+    sum=(n/2)*(2*a+(n-1)*d)                                     #sum of all terms
+    Progression=Label(root, text=series).place(x=100,y=250)     #placing the series
+    sum_of_terms=Label(root,text=sum).place(x=150,y=300)        #placing the sum of series
+#Function to calculate the sum and series of an gp 
+def gp():
+    a=float(entry1.get())                                       #a=first term
+    d=float(entry2.get())                                       #d=increment
+    n=float(entry3.get())                                       #n=number of terms
+    f=0                                                         #the 'fth' term in the series
+    series=[]                                                   #empty series
+    series.append(a*(d**f))                                     #first term
+    while (f+1) != n:                                           #goes through the terms
+        f=f+1                                                   #increments through the terms
+        series.append(",")                                      #separates the terms
+        series.append(a*(d**f))                                 #term inputted in the series
+    sum=(a*((d**n)-1))/(d-1)                                    #sum of all terms
+    Progression=Label(root, text=series).place(x=100,y=250)     #placing the series
+    sum_of_terms=Label(root,text=sum).place(x=150,y=300)        #placing the sum of series
+    
+#Creating radiobuttons for user to choose between ap and gp
+radio1=Radiobutton(root,text="Arithmetic Progression",command=ap)
+radio2=Radiobutton(root,text="Geometric Progression",command=gp)
 
-
-
-
-
+#Placing radiobuttons on window
+radio1.place(x=50,y=200)
+radio2.place(x=200,y=200)
 
 root.mainloop()
