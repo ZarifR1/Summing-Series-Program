683e9b23d482013105b201e0d5150b216a8460f7
Shayem1 <shayen.quazi@education.nsw.gov.au>
2023-08-10 11:18:09 +1000

    deleting other branches

--- Summing-Series.py
+++ Summing-Series.py
@@ -48,6 +48,7 @@ def summing_series():
         entry2.configure(bg_color=bg_colour, fg_color=fg_colour)
         entry3.configure(bg_color=bg_colour, fg_color=fg_colour)
         sequence.configure(bg_color=bg_colour, fg_color=fg_colour)
+        label7.configure(bg_color=bg_colour, fg_color=fg_colour)
         space=customtkinter.CTkLabel(root,text="                                                                      ", bg_color=bg_colour, fg_color=bg_colour).place(x=x6,y=y8)
     
         
@@ -96,57 +97,18 @@ def summing_series():
     entry2.place(x=x2,y=y2)
     entry3.place(x=x2,y=y3)
     
-    def First_term_input(number):
-        current=entry1.get()
-        entry1.delete()
-        entry1.insert(0,str(current)+str(number))
-
-    def Increment_input(number):
-        current=entry2.get()
-        entry2.delete()
-        entry2.insert(0,str(current)+str(number))
-
-    def Num_of_terms_input(number):
-        current=entry3.get()
-        entry3.delete()
-        entry3.insert(0,str(current)+str(number))
-
     def choose_first_term(number):
-        key_1.configure(command=lambda: First_term_input(1))
-        key_2.configure(command=lambda: First_term_input(2))
-        key_3.configure(command=lambda: First_term_input(3))
-        key_4.configure(command=lambda: First_term_input(4))
-        key_5.configure(command=lambda: First_term_input(5))
-        key_6.configure(command=lambda: First_term_input(6))
-        key_7.configure(command=lambda: First_term_input(7))
-        key_8.configure(command=lambda: First_term_input(8))
-        key_9.configure(command=lambda: First_term_input(9))
-        key_0.configure(command=lambda: First_term_input(0))
+        pass
 
     def choose_increment():
-        key_1.configure(command=lambda: Increment_input(1))
-        key_2.configure(command=lambda: Increment_input(2))
-        key_3.configure(command=lambda: Increment_input(3))
-        key_4.configure(command=lambda: Increment_input(4))
-        key_5.configure(command=lambda: Increment_input(5))
-        key_6.configure(command=lambda: Increment_input(6))
-        key_7.configure(command=lambda: Increment_input(7))
-        key_8.configure(command=lambda: Increment_input(8))
-        key_9.configure(command=lambda: Increment_input(9))
-        key_0.configure(command=lambda: Increment_input(0))
+        pass
 
     def choose_num_terms():
-        key_1.configure(command=lambda: Num_of_terms_input(1))
-        key_2.configure(command=lambda: Num_of_terms_input(2))
-        key_3.configure(command=lambda: Num_of_terms_input(3))
-        key_4.configure(command=lambda: Num_of_terms_input(4))
-        key_5.configure(command=lambda: Num_of_terms_input(5))
-        key_6.configure(command=lambda: Num_of_terms_input(6))
-        key_7.configure(command=lambda: Num_of_terms_input(7))
-        key_8.configure(command=lambda: Num_of_terms_input(8))
-        key_9.configure(command=lambda: Num_of_terms_input(9))
-        key_0.configure(command=lambda: Num_of_terms_input(0))
+        pass
 
+    entry4=customtkinter.CTkEntry(root, corner_radius=6, bg_color=bg_colour)
+    entry4.place(x=675, y =280)
+    
     label8=customtkinter.CTkLabel(root,text="Choose the entry box",bg_color=bg_colour,fg_color=fg_colour,corner_radius=6)                         #   Entry box selection label
     label8.place(x=650,y=50)
 
